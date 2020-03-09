00e99c004c84051b19628fdc35dad0e5f9bac8fa
Natalia Mateo <nmateo7@gmail.com>
2020-03-09 16:18:26 +0100

    change main.js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,23 +1,14 @@
 "use strict";
 
-const userDetails = {};
-const fullname = document.querySelector(".js-fullname");
-const job = document.querySelector(".js-job");
-const email = document.querySelector(".js-email");
-const telephone = document.querySelector(".js-telephone");
-const linkedin = document.querySelector(".js-linkedin");
-const github = document.querySelector(".js-github");
+const userDetails = document.querySelectorAll(".js-input-text");
+
+// un bucle que recorra todos los elementos del objeto
 
 function handleUserDetails(ev) {
   debugger;
   const el = ev.currentTarget;
   userDetails[name] = el.value;
 }
-fullname.addEventListener("keyup", handleUserDetails);
-job.addEventListener("keyup", handleUserDetails);
-email.addEventListener("keyup", handleUserDetails);
-telephone.addEventListener("keyup", handleUserDetails);
-linkedin.addEventListener("keyup", handleUserDetails);
-github.addEventListener("keyup", handleUserDetails);
+userDetails.addEventListener("keyup", handleUserDetails);
 
 console.log(userDetails);
