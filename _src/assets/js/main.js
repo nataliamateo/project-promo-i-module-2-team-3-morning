6259a8782f15c67c8ab3166156d778b6bf4dc6ce
Natalia Mateo <nmateo7@gmail.com>
2020-03-09 16:56:29 +0100

    Create functions

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,14 +1,35 @@
 "use strict";
+debugger;
 
-const userDetails = document.querySelectorAll(".js-input-text");
+const copyName = document.querySelector(".js-copyname");
+const pasteName = document.querySelector(".js-pastename");
+const copyJob = document.querySelector(".js-copyjob");
+const pasteJob = document.querySelector(".js-pastejob");
 
-// un bucle que recorra todos los elementos del objeto
-
-function handleUserDetails(ev) {
-  debugger;
-  const el = ev.currentTarget;
-  userDetails[name] = el.value;
+function addText(ev) {
+  pasteName.innerHTML = event.currentTarget.value;
+  console.log(pasteName.innerHTML);
 }
-userDetails.addEventListener("keyup", handleUserDetails);
+copyName.addEventListener("keyup", addText);
 
-console.log(userDetails);
+function addJob(ev) {
+  pasteJob.innerHTML = event.currentTarget.value;
+}
+
+copyJob.addEventListener("keyup", addJob);
+
+// const userDetails = document.querySelectorAll(".js-input-text");
+
+// // un bucle que recorra todos los elementos del objeto
+
+// for (const details of userDetails) {
+//   console.log(`${details}`);
+// }
+
+// function handleUserDetails(ev) {
+//   debugger;
+//   const el = ev.currentTarget;
+//   userDetails[name] = el.value;
+// }
+
+// userDetails.addEventListener("keyup", handleUserDetails);
