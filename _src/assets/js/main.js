9e3a1d4712b89915b620c7e6191f232ddb1b47cf
Natalia Mateo <nmateo7@gmail.com>
2020-03-12 18:05:31 +0100

    Resolve conflicts with forms

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,33 +1,33 @@
-'use strict';
+"use strict";
 debugger;
 
-const copyName = document.querySelector('.js-copyname');
-const pasteName = document.querySelector('.js-pastename');
-const copyJob = document.querySelector('.js-copyjob');
-const pasteJob = document.querySelector('.js-pastejob');
+const copyName = document.querySelector(".js-copyname");
+const pasteName = document.querySelector(".js-pastename");
+const copyJob = document.querySelector(".js-copyjob");
+const pasteJob = document.querySelector(".js-pastejob");
 
 function addText(ev) {
   pasteName.innerHTML = event.currentTarget.value;
   console.log(pasteName.innerHTML);
 }
-copyName.addEventListener('keyup', addText);
+copyName.addEventListener("keyup", addText);
 
 function addJob(ev) {
   pasteJob.innerHTML = event.currentTarget.value;
 }
 
-copyJob.addEventListener('keyup', addJob);
+copyJob.addEventListener("keyup", addJob);
 
 // botón reset
-const imageName = document.querySelector('.js-pastename');
-const imageJob = document.querySelector('.js-pastejob');
-const imageButtonReset = document.querySelector('.js-reset');
+const imageName = document.querySelector(".js-pastename");
+const imageJob = document.querySelector(".js-pastejob");
+const imageButtonReset = document.querySelector(".js-reset");
 
 function resetImageData() {
-  imageName.innerHTML = 'Nombre Apellido';
-  imageJob.innerHTML = 'Front-end developer';
+  imageName.innerHTML = "Nombre Apellido";
+  imageJob.innerHTML = "Front-end developer";
 }
-imageButtonReset.addEventListener('click', resetImageData);
+imageButtonReset.addEventListener("click", resetImageData);
 
 // const userDetails = document.querySelectorAll(".js-input-text");
 
@@ -46,23 +46,23 @@ imageButtonReset.addEventListener('click', resetImageData);
 // userDetails.addEventListener("keyup", handleUserDetails);
 
 //start collapsible
-const collapsibleTrigger = document.querySelectorAll('.js-collapsible--trigger');
+// const collapsibleTrigger = document.querySelectorAll('.js-collapsible--trigger');
 
 function updateCollapsible(ev) {
   const currentCollapsible = ev.currentTarget.parentElement;
-  if (currentCollapsible.classList.contains('collapsable--open')) {
+  if (currentCollapsible.classList.contains("collapsable--open")) {
     // Si está abierto, lo puedo cerrar
-    currentCollapsible.classList.remove('collapsable--open');
+    currentCollapsible.classList.remove("collapsable--open");
   } else {
     // Si está cerrado:
     // Los cierro todos
     for (const item of collapsibleTrigger) {
-      item.parentElement.classList.remove('collapsable--open');
+      item.parentElement.classList.remove("collapsable--open");
     }
     // Abro el correcto
-    currentCollapsible.classList.add('collapsable--open');
+    currentCollapsible.classList.add("collapsable--open");
   }
 }
 for (const item of collapsibleTrigger) {
-  item.addEventListener('click', updateCollapsible);
+  item.addEventListener("click", updateCollapsible);
 }
