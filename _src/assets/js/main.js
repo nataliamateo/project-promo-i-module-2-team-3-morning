3ccb50bf499ceba497bd4975db01d3f7a16f85ed
BelenCatalan <labdelante@gmail.com>
2020-03-15 14:49:55 +0100

    add reset button

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,5 +1,4 @@
 'use strict';
-debugger;
 
 const copyName = document.querySelector('.js-copyname');
 const pasteName = document.querySelector('.js-pastename');
@@ -18,16 +17,34 @@ function addJob(ev) {
 
 copyJob.addEventListener('keyup', addJob);
 
-// botón reset
+//BOTON RESET
+//necesitamos recorrer todos los imputs, y poner todos sus values a '', userDetailsoff.value = "";, tocando solo el value, los placeholder igual se quedan.
+// cuanto se tenga el objeto con los imputs, lo que hay que hacer es recorrer ese obejto y poner sus valores a '';
 const imageName = document.querySelector('.js-pastename');
 const imageJob = document.querySelector('.js-pastejob');
 const imageButtonReset = document.querySelector('.js-reset');
 
-function resetImageData() {
+function resetUserData() {
+  // for (const userDetailson of userDetails) { //userDetails es el objeto que creamos con los datos
+  //   userDetailson.value = '';
+  // } //esto solo resetea los valores del objeto, pero también resetea los imputs? no verdad?
+  const allInputData = document.querySelectorAll('.js-input-text');
+  console.log(allInputData);
+  for (const allInputDataOff of allInputData) {
+    console.log(allInputDataOff);
+    allInputDataOff.value = '';
+  }
+
   imageName.innerHTML = 'Nombre Apellido';
   imageJob.innerHTML = 'Front-end developer';
+  cardElement.classList.remove('js-card-palette2');
+  cardElement.classList.remove('js-card-palette3');
+  cardElement.classList.add('js-card-palette1');
+  console.log(cardElement);
 }
-imageButtonReset.addEventListener('click', resetImageData);
+imageButtonReset.addEventListener('click', resetUserData);
+
+//AQUI ACABA BOTON RESET
 
 // const userDetails = document.querySelectorAll(".js-input-text");
 
