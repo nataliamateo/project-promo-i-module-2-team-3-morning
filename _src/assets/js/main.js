65471bf1fb85f958a9df8a46e3d491cdf3e7ff7e
BelenCatalan <labdelante@gmail.com>
2020-03-18 16:49:02 +0100

    send to localStorage almost complet

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // const userData = {
 //  palette:"",
@@ -13,29 +13,32 @@
 //BOTON RESET
 //necesitamos recorrer todos los imputs, y poner todos sus values a '', userDetailsoff.value = "";, tocando solo el value, los placeholder igual se quedan.
 // cuanto se tenga el objeto con los imputs, lo que hay que hacer es recorrer ese obejto y poner sus valores a '';
-const imageName = document.querySelector(".js-pastename");
-const imageJob = document.querySelector(".js-pastejob");
-const imageButtonReset = document.querySelector(".js-reset");
+const imageName = document.querySelector('.js-pastename');
+const imageJob = document.querySelector('.js-pastejob');
+const imageButtonReset = document.querySelector('.js-reset');
 
 function resetUserData() {
   // for (const userDetailson of userDetails) { //userDetails es el objeto que creamos con los datos
   //   userDetailson.value = '';
   // } //esto solo resetea los valores del objeto, pero también resetea los imputs? no verdad?
-  const allInputData = document.querySelectorAll(".js-input-text");
+  const allInputData = document.querySelectorAll('.js-input-text');
   console.log(allInputData);
   for (const allInputDataOff of allInputData) {
     console.log(allInputDataOff);
-    allInputDataOff.value = "";
+    allInputDataOff.value = '';
   }
-
-  document.querySelector(".js-cardname").innerHTML = "Nombre Apellido";
-  document.querySelector(".js-cardjob").innerHTML = "Front-end developer";
-  cardElement.classList.remove("js-card-palette2");
-  cardElement.classList.remove("js-card-palette3");
-  cardElement.classList.add("js-card-palette1");
+  // reseteo las paletas
+  document.querySelector('.js-palette1 .js-palette1').checked = true;
+  cardElement.classList.remove('js-card-palette2');
+  cardElement.classList.remove('js-card-palette3');
+  cardElement.classList.add('js-card-palette1');
   console.log(cardElement);
+  // propago los datos desde el formulario hasta la card y el local storage
+  getUserName();
+  paintPalette();
 }
-imageButtonReset.addEventListener("click", resetUserData);
+
+imageButtonReset.addEventListener('click', resetUserData);
 
 //AQUI ACABA BOTON RESET
 
@@ -56,107 +59,106 @@ imageButtonReset.addEventListener("click", resetUserData);
 // userDetails.addEventListener("keyup", handleUserDetails);
 
 //START COLLAPSIBLE
-const collapsibleTrigger = document.querySelectorAll(".js-collapsible--trigger");
-const collapsibleTrigger2 = document.querySelector(".js-collapsible--trigger--2");
+const collapsibleTrigger = document.querySelectorAll('.js-collapsible--trigger');
+const collapsibleTrigger2 = document.querySelector('.js-collapsible--trigger--2');
 console.log(collapsibleTrigger2);
 
 function updateCollapsible(ev) {
   const currentCollapsible = ev.currentTarget.parentElement;
-  if (currentCollapsible.classList.contains("collapsable--open")) {
+  if (currentCollapsible.classList.contains('collapsable--open')) {
     // Si está abierto, lo puedo cerrar
-    currentCollapsible.classList.remove("collapsable--open");
+    currentCollapsible.classList.remove('collapsable--open');
   } else {
     // Si está cerrado:
     // Los cierro todos
     for (const item of collapsibleTrigger) {
-      item.parentElement.classList.remove("collapsable--open");
+      item.parentElement.classList.remove('collapsable--open');
     }
     // Abro el correcto
-    currentCollapsible.classList.add("collapsable--open");
+    currentCollapsible.classList.add('collapsable--open');
   }
 }
 for (const item of collapsibleTrigger) {
-  item.addEventListener("click", updateCollapsible);
+  item.addEventListener('click', updateCollapsible);
 }
 
 function updateCollapsibleTwitter(ev) {
   const currentCollapsibleTwitter = ev.currentTarget.parentElement;
-  if (currentCollapsibleTwitter.classList.contains("collapsable--open2")) {
-    currentCollapsibleTwitter.classList.remove("collapsable--open2");
+  if (currentCollapsibleTwitter.classList.contains('collapsable--open2')) {
+    currentCollapsibleTwitter.classList.remove('collapsable--open2');
   } else {
-    currentCollapsibleTwitter.classList.add("collapsable--open2");
+    currentCollapsibleTwitter.classList.add('collapsable--open2');
   }
 }
-collapsibleTrigger2.addEventListener("click", updateCollapsibleTwitter);
+collapsibleTrigger2.addEventListener('click', updateCollapsibleTwitter);
 
 // COLORES DE LA PALETA
 
-const inputPalette1 = document.querySelector(".js-palette1");
-const inputPalette2 = document.querySelector(".js-palette2");
-const inputPalette3 = document.querySelector(".js-palette3");
-const cardElement = document.querySelector(".js-card");
+const inputPalette1 = document.querySelector('.js-palette1');
+const inputPalette2 = document.querySelector('.js-palette2');
+const inputPalette3 = document.querySelector('.js-palette3');
+const cardElement = document.querySelector('.js-card');
 
 function paintPalette() {
-  cardElement.classList.remove("js-card-palette1");
-  cardElement.classList.remove("js-card-palette2");
-  cardElement.classList.remove("js-card-palette3");
-  let selectedPalette = document.querySelector(".js-radio:checked");
+  cardElement.classList.remove('js-card-palette1');
+  cardElement.classList.remove('js-card-palette2');
+  cardElement.classList.remove('js-card-palette3');
+  let selectedPalette = document.querySelector('.js-radio:checked');
   console.log(selectedPalette);
   selectedPalette = selectedPalette.value;
   console.log(selectedPalette);
-  cardElement.classList.add("js-card-palette" + selectedPalette);
+  cardElement.classList.add('js-card-palette' + selectedPalette);
+  localStorage.setItem('palette', selectedPalette);
 }
 
-inputPalette1.addEventListener("click", paintPalette);
-inputPalette2.addEventListener("click", paintPalette);
-inputPalette3.addEventListener("click", paintPalette);
+inputPalette1.addEventListener('click', paintPalette);
+inputPalette2.addEventListener('click', paintPalette);
+inputPalette3.addEventListener('click', paintPalette);
 
 // AQUÍ ACABAN COLORES DE LA PALETA
 
 // FORMULARIO RELLENA REALIZADO DE MANERA LARGA
-const userName = document.querySelector(".js-input-name");
-const userJob = document.querySelector(".js-input-job");
-const userMail = document.querySelector(".js-input-email");
-const userPhone = document.querySelector(".js-input-phone");
-const userLinkedin = document.querySelector(".js-input-linkedin");
-const userGithub = document.querySelector(".js-input-github");
-
-const printJob = document.querySelector(".js-cardjob");
+const userName = document.querySelector('.js-input-name');
+const userJob = document.querySelector('.js-input-job');
+const userMail = document.querySelector('.js-input-email');
+const userPhone = document.querySelector('.js-input-phone');
+const userLinkedin = document.querySelector('.js-input-linkedin');
+const userGithub = document.querySelector('.js-input-github');
 
 function getUserName() {
-  document.querySelector(".js-cardname").innerHTML = userName.value;
-  localStorage.setItem("name", userName.value);
+  document.querySelector('.js-cardname').innerHTML = userName.value || 'Nombre Apellido';
+  localStorage.setItem('name', userName.value);
 }
 
 function getUserJob() {
-  printJob.innerHTML = userJob.value;
-  localStorage.setItem("job", userJob.value);
+  document.querySelector('.js-cardjob').innerHTML = userJob.value || 'Front end developer';
+  localStorage.setItem('job', userJob.value);
 }
 function getUserMail() {
-  document.querySelector(".js-cardemail").href = "mailto:" + userMail.value;
-  localStorage.setItem("mail", userMail.value);
+  document.querySelector('.js-cardemail').href = 'mailto:' + userMail.value;
+  localStorage.setItem('mail', userMail.value);
 }
 
 function getUserPhone() {
-  document.querySelector(".js-cardphone").href = "tel:" + userPhone.value;
-  localStorage.setItem("phone", userPhone.value);
+  document.querySelector('.js-cardphone').href = 'tel:' + userPhone.value;
+  localStorage.setItem('phone', userPhone.value);
 }
 function getUserLinkedin() {
-  document.querySelector(".js-cardlinkedin").href + userLinkedin.value;
-  localStorage.setItem("linkedin", userLinkedin.value);
+  document.querySelector('.js-cardlinkedin').href + userLinkedin.value;
+  localStorage.setItem('linkedin', userLinkedin.value);
 }
 
 function getUserGithub() {
-  document.querySelector(".js-cardgithub").href = "" + userGithub.value;
-  localStorage.setItem("github", userMail.value);
+  document.querySelector('.js-cardgithub').href = '' + userGithub.value;
+  localStorage.setItem('github', userMail.value);
 }
 
-userName.addEventListener("keyup", getUserName);
-userJob.addEventListener("keyup", getUserJob);
-userMail.addEventListener("keyup", getUserMail);
-userPhone.addEventListener("keyup", getUserPhone);
-userLinkedin.addEventListener("keyup", getUserLinkedin);
-userGithub.addEventListener("keyup", getUserGithub);
+userName.addEventListener('keyup', getUserName);
+userJob.addEventListener('keyup', getUserJob);
+userMail.addEventListener('keyup', getUserMail);
+userPhone.addEventListener('keyup', getUserPhone);
+userLinkedin.addEventListener('keyup', getUserLinkedin);
+userGithub.addEventListener('keyup', getUserGithub);
 
 // const saveUserData = localStorage.getItem("userData");
 // const saveUserData = localStorage.getItem("job");
@@ -174,10 +176,10 @@ userGithub.addEventListener("keyup", getUserGithub);
 
 // AÑADIR IMAGEN
 const fr = new FileReader();
-const uploadBtn = document.querySelector(".js__profile-trigger");
-const fileField = document.querySelector(".js__profile-upload-btn");
-const profileImage = document.querySelector(".js__profile-image");
-const profilePreview = document.querySelector(".js__profile-preview");
+const uploadBtn = document.querySelector('.js__profile-trigger');
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js__profile-preview');
 
 /**
  * Recoge el archivo añadido al campo de tipo "file"
@@ -189,7 +191,7 @@ const profilePreview = document.querySelector(".js__profile-preview");
  */
 function getImage(e) {
   var myFile = e.currentTarget.files[0];
-  fr.addEventListener("load", writeImage);
+  fr.addEventListener('load', writeImage);
   fr.readAsDataURL(myFile);
 }
 
@@ -218,7 +220,49 @@ function fakeFileClick() {
  * - al botón visible para generar el click automático
  * - al campo oculto para cuando cambie su value
  */
-uploadBtn.addEventListener("click", fakeFileClick);
-fileField.addEventListener("change", getImage);
+uploadBtn.addEventListener('click', fakeFileClick);
+fileField.addEventListener('change', getImage);
 
 // FINAL AÑADIR IMAGEN
+// Recuperar datos del localStorage
+function getDataFromLocalStorage() {
+  const userNameValue = localStorage.getItem('name');
+  if (userNameValue !== null) {
+    userName.value = userNameValue;
+  }
+  const paletteValue = localStorage.getItem('palette');
+  if (paletteValue !== null) {
+    document.querySelector(`.js-palette${paletteValue} .js-palette${paletteValue}`).checked = true;
+  }
+  // job...
+  const userJobValue = localStorage.getItem('job');
+  if (userJobValue !== null) {
+    userJob.value = userJobValue;
+  }
+  // photo
+  //email
+  const userMailValue = localStorage.getItem('mail');
+  if (userMailValue !== null) {
+    userMail.value = userMailValue;
+  }
+  //Phone
+  const userPhoneValue = localStorage.getItem('phone');
+  if (userPhoneValue !== null) {
+    userPhone.value = userPhoneValue;
+  }
+  //Linkedin
+  const userLinkedinValue = localStorage.getItem('linkedin');
+  if (userLinkedinValue !== null) {
+    userLinkedin.value = userLinkedinValue;
+  }
+  //GitHub
+  const userGithubValue = localStorage.getItem('github');
+  if (userGithubValue !== null) {
+    userGithub.value = userGithubValue;
+  }
+}
+getDataFromLocalStorage();
+
+paintPalette();
+getUserName();
+getUserJob();
